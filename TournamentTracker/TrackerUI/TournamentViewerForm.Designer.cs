e924e51a2ce2eb5838aa98bcaeca495f653fabdb
Will Kerwin <10259319+Dr-gummybears@users.noreply.github.com>
2019-05-30 01:06:52 +0100

    Got Tired Going To Bed but ehh i know this stuff dont know inner workings

---
+++ TournamentTracker/TrackerUI/TournamentViewerForm.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace TrackerUI
+{
+    partial class TournamentViewer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LBLHeader = new System.Windows.Forms.Label();
+            this.LBL = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // LBLHeader
+            // 
+            this.LBLHeader.AutoSize = true;
+            this.LBLHeader.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(5)))), ((int)(((byte)(51)))), ((int)(((byte)(255)))));
+            this.LBLHeader.Location = new System.Drawing.Point(12, 9);
+            this.LBLHeader.Name = "LBLHeader";
+            this.LBLHeader.Size = new System.Drawing.Size(129, 30);
+            this.LBLHeader.TabIndex = 0;
+            this.LBLHeader.Text = "Tournament:";
+            // 
+            // LBL
+            // 
+            this.LBL.AutoSize = true;
+            this.LBL.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(5)))), ((int)(((byte)(51)))), ((int)(((byte)(255)))));
+            this.LBL.Location = new System.Drawing.Point(147, 9);
+            this.LBL.Name = "LBL";
+            this.LBL.Size = new System.Drawing.Size(88, 30);
+            this.LBL.TabIndex = 1;
+            this.LBL.Text = "<none>";
+            // 
+            // TournamentViewer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 30F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(599, 401);
+            this.Controls.Add(this.LBL);
+            this.Controls.Add(this.LBLHeader);
+            this.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.Name = "TournamentViewer";
+            this.Text = "Tournament Viewer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LBLHeader;
+        private System.Windows.Forms.Label LBL;
+    }
+}
+
