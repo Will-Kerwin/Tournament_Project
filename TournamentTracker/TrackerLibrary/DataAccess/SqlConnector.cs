8981c6b0a2405cace2583751fea805f0c4c60ae4
Will Kerwin <10259319+Dr-gummybears@users.noreply.github.com>
2019-07-04 22:16:59 +0100

    code cleanup folders and namespaces

--- TournamentTracker/TrackerLibrary/DataAccess/SqlConnector.cs
+++ TournamentTracker/TrackerLibrary/DataAccess/SqlConnector.cs
@@ -1,10 +1,14 @@
-﻿namespace TrackerLibrary
+﻿using TrackerLibrary.Models;
+
+namespace TrackerLibrary.DataAccess
 {
     public class SqlConnector : IDataConnection
     {
         // TODO - Make the create prize method actually save to the database
         /// <summary>
         /// Saves a new prize to the database
+        /// 
+        /// we are going to be using dapper to connect to sql 
         /// </summary>
         /// <param name="model"></param>
         /// <returns>the prize information, including the unique identifier</returns>
--- TournamentTracker/TrackerLibrary/DataAccess/TextConnector.cs
+++ TournamentTracker/TrackerLibrary/DataAccess/TextConnector.cs
@@ -1,8 +1,15 @@
-﻿namespace TrackerLibrary
+﻿using TrackerLibrary.Models;
+
+namespace TrackerLibrary.DataAccess
 {
     public class TextConnector : IDataConnection
     {
         // TODO - Wire up the Create Prize for text files.
+        /// <summary>
+        /// Saves data to file
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
         public PrizeModel CreatePrize(PrizeModel model)
         {
             model.Id = 1;
